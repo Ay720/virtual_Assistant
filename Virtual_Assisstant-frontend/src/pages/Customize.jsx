51342bea94ce2a5a0c2ec75720b09b66465b25a1
AYUSH RAJ <148180288+Ay720@users.noreply.github.com>
2026-06-18 20:11:09 +0530

    Update Customize.jsx

--- Virtual_Assisstant-frontend/src/pages/Customize.jsx
+++ Virtual_Assisstant-frontend/src/pages/Customize.jsx
@@ -1,5 +1,5 @@
 import React, { useRef, useEffect, useContext } from "react";
-import { UserDataContext } from "../context/userContext";
+import { UserDataContext } from "../context/UserContext";
 import Card from "../components/cards";
 import { GrDocumentUpload } from "react-icons/gr";
 import image1 from "../assets/image1.png";
