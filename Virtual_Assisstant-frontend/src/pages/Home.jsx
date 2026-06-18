a52b928d887621f3539490fa04167699347b77b0
AYUSH RAJ <148180288+Ay720@users.noreply.github.com>
2026-06-18 20:11:46 +0530

    Update Home.jsx

--- Virtual_Assisstant-frontend/src/pages/Home.jsx
+++ Virtual_Assisstant-frontend/src/pages/Home.jsx
@@ -1,5 +1,5 @@
 import { useContext, useEffect } from "react";
-import { UserDataContext } from "../context/userContext";
+import { UserDataContext } from "../context/UserContext";
 import { useNavigate } from "react-router-dom";
 import axios from "axios";
 
