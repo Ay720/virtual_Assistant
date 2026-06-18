812772cae14b1868ca02372ab52b3009166e92f0
AYUSH RAJ <148180288+Ay720@users.noreply.github.com>
2026-06-18 20:14:46 +0530

    Update cards.jsx

--- Virtual_Assisstant-frontend/src/components/cards.jsx
+++ Virtual_Assisstant-frontend/src/components/cards.jsx
@@ -1,5 +1,5 @@
 import React, { useContext } from "react";
-import { UserDataContext } from "../context/userContext";
+import { UserDataContext } from "../context/UserContext";
 
 const Card = ({ img, className }) => {
   const { selectedImage, setSelectedImage,frontendImage,setFrontendImage,backendImage,setBackendImage } = useContext(UserDataContext);
